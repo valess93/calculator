938b4c63d3833fa61a5ef4d9cd65268877b48c44
Valentina Moroni <spam.moroni@gmail.com>
2015-05-15 18:42:50 +0200

    First commit

---
+++ calculator.rb
@@ -0,0 +1,22 @@
+def is_number?(object)
+  true if Float(object) rescue false
+end
+
+PI = 3.1415
+
+puts "Calculator"
+puts "PI is " + PI.to_s
+
+a = gets.chomp
+b = gets.chomp
+
+if(is_number?(a) && is_number?(b))
+  puts "Correct input"
+
+  number_a = a.to_i
+  number_b = b.to_i
+
+  puts number_a + number_b
+else
+  puts "This is wrong"
+end
